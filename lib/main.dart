6fa9670ebc1eecea7cb09772b505cfb007a6ce3a
Xavier Francisco <xavierfrancisco353@gmail.com>
2023-07-25 00:18:13 +0200

    Setup parse server

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_classified/screens/screens.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:parse_server_sdk/parse_server_sdk.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+  await dotenv.load(fileName: ".env");
+  final keyApplicationId = dotenv.env['KEY_APPLICATION_ID'];
+  final keyParseClientKey = dotenv.env['KEY_CLIENT_KEY'];
+  final keyParseServerUrl = dotenv.env['KEY_PARSE_SERVER_URL'];
+  await Parse().initialize(keyApplicationId!, keyParseServerUrl!,
+      clientKey: keyParseClientKey,
+      debug: true,
+      //   liveQueryUrl: "keyLiveQueryUrl",
+      autoSendSessionId: true,
+      coreStore: CoreStoreMemoryImp());
+
+  final category = ParseObject('Categories')
+    ..set('Title', 'Camisetes')
+    ..set('Position', 2);
+  final response = await category.save();
+
+  print(response.success);
 }
 
 class MyApp extends StatelessWidget {
